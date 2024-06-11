03ba5804a400fba3ec9af182a57bfa04e972e78e
Neo Choi <neo.choi@gropyus.com>
2024-06-12 01:13:42 +0200

    feat: update language

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import 'style/main.css';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="zh-Hant">
+    <html lang="zh">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
